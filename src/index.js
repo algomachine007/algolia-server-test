510c336b96e6d5feb9e2f0a77a94a8a6f7df7f62
algomachine007 <amaben202phantom@gmail.com>
2023-01-18 18:18:25 +0100

    first commit 2

--- src/index.js
+++ src/index.js
@@ -7,8 +7,20 @@ const PORT = process.env.PORT || 8081;
 app.use(bodyParser.json());
 
 app.post("/a", (req, res) => {
+  console.log("Req Object", req);
+
   const { info } = req.body;
   res.status(200).send(info);
+});
+
+app.post("/b", (req, res) => {
+  console.log("Req Object", req);
+
+  res.status(200).json(req.body);
+});
+
+app.get("/c", (req, res) => {
+  res.status(200).send("Working");
 });
 
 app.listen(PORT, () => {
